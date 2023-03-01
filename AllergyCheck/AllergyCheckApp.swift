7592ab9cc6359e1a37c88f4ab6d4bb369c4e3b16
Ruby Feller <ruby.feller@student.fontys.nl>
2023-03-01 13:29:59 +0100

    Initial Commit

---
+++ AllergyCheck/AllergyCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllergyCheckApp.swift
+//  AllergyCheck
+//
+//  Created by Ruby Feller on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AllergyCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
