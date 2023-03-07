28e289b8b6edd164eb8f8aa2716d50737ea57293
Ruby Feller <ruby.feller@student.fontys.nl>
2023-03-07 10:48:05 +0100

    Allergies now retrieved via JSON-file

---
+++ AllergyCheck/Allergy.swift
@@ -0,0 +1,19 @@
+//
+//  Allergy.swift
+//  AllergyCheck
+//
+//  Created by Ruby Feller on 07/03/2023.
+//
+
+import Foundation
+
+struct Allergy: Codable {
+    let name: String
+    let icon: String
+    let description: String
+    let symptoms: [String]
+}
+
+struct AllergiesData: Codable {
+    let allergies: [Allergy]
+}
--- AllergyCheck/AllergySelectionView.swift
+++ AllergyCheck/AllergySelectionView.swift
@@ -9,58 +9,61 @@ import SwiftUI
 
 struct AllergySelectionView: View {
     
-    struct Allergy: Codable {
-        var name: String
-        var icon: String
-    }
-    
     private let range: ClosedRange<Int> = 0...8
     @State private var selectedItems: [Int] = []
     
-    var body: some View {
-        TabView {
-
-            VStack {
-                Text("Choose your allergies")
-                    .foregroundColor(.white)
-                    .padding(50)
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
-                    ForEach(range, id: \.self) { index in
-                        VStack {
-                            Button("🥜") {
-                                if let selectedItemIndex = self.selectedItems.firstIndex(of: index) {
-                                    selectedItems.remove(at: selectedItemIndex)
-                                    print("removed \(index)")
-                                } else {
-                                    self.selectedItems.append(index)
-                                    print("added \(index)")
-                                    print(selectedItems)
-                                }
-                            }
-                            .padding()
-                            .background(self.selectedItems.contains(index) ? Color("Tertiary") : Color("Secondary"))
-                            .shadow(radius: 30)
-                            .cornerRadius(10)
-                            Text("Nuts")
-                                .font(.caption)
-                        }
-                    }
-                    
-                }
-                Button("Continue") {
-                }
-                .buttonStyle(.borderedProminent)
-                .tint(Color("Secondary"))
-                .controlSize(.large)
-                .fontWeight(.bold)
-            }
-            .frame(maxWidth: .infinity)
-            .frame(maxHeight: .infinity)
-            .background(Color("Primary"))
-            .tabItem {
-                Label("Test", systemImage: "barcode.viewfinder")
-            }
+    let allergies: [Allergy]
+    
+    init() {
+        if let allergiesData: AllergiesData = Bundle.main.decode("allergies.json") {
+            self.allergies = allergiesData.allergies
+        } else {
+            self.allergies = []
         }
+    }
+    
+    var body: some View {
+        
+        VStack {
+            Text("Choose your allergies")
+                .foregroundColor(.white)
+                .padding(50)
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+                ForEach(allergies.indices, id: \.self) { index in
+                    VStack {
+                        Button(allergies[index].icon) {
+                            if let selectedItemIndex = self.selectedItems.firstIndex(of: index) {
+                                selectedItems.remove(at: selectedItemIndex)
+                                print("removed \(index)")
+                            } else {
+                                self.selectedItems.append(index)
+                                print("added \(index)")
+                                print(selectedItems)
+                            }
+                        }
+                        .padding()
+                        .background(self.selectedItems.contains(index) ? Color("Tertiary") : Color("Secondary"))
+                        .shadow(radius: 30)
+                        .cornerRadius(10)
+                        Text(allergies[index].name)
+                            .font(.caption)
+                    }
+                }
+                
+            }
+            
+            Button("Continue") {
+                UserDefaults.standard.set(selectedItems, forKey: "Allergies")
+                print(selectedItems)
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(Color("Secondary"))
+            .controlSize(.large)
+            .fontWeight(.bold)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(maxHeight: .infinity)
+        .background(Color("Primary"))
     }
 }
 
